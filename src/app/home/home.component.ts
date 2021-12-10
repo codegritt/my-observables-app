c1abb3e3e8e5aab7048398a7b44d5afe5668f87a
Gokul <gokulchandan18@gmail.com>
2021-12-10 16:33:18 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private first0bsSubscription: Subscription = new Subscription;
+ constructor() { }
+ const customIntervalObservable = Observable.create(observer => {
+  let count = 0;
+  setInterval(() => {
+    observer.next(count);
+    if (count === 5) {
+      observer.complete();
+    }
+    if (count > 3) {
+      observer.error(new Error('Count is greater 3!'));
+    }
+    count++;
+  }, 1000);
+});
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+  return data > 0;
+}), map((data: number) => {
+  return 'Round: ' + (data + 1);
+})).subscribe(data => {
+  console.log(data);
+}, error => {
+  console.log(error);
+  alert(error.message);
+}, () => {
+  console.log('Completed!');
+});
+}
+
+ngOnDestroy(): void {
+this.firstObsSubscription.unsubscribe();
+}
 
 }
